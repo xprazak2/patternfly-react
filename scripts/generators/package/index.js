89b7a82cac3456020c94c0bf8a1168a66da8d322
Jeffrey Phillips <jephilli@redhat.com>
2018-08-29 17:24:39 -0400

    Revert "refactor(Move patternfly-3 and patternfly-4 specific packages into re… (#570)"
    This reverts commit aa839a80a005f5d540faeb0e1a0320b7df1cc982.

--- scripts/generators/package/index.js
+++ scripts/generators/package/index.js
@@ -4,11 +4,6 @@ const { join, resolve } = require('path');
 const { REMOVE_NPM_SCOPE } = require('../helpers');
 
 const packagesRoot = resolve(__dirname, '../../../packages');
-const pf3Root = join(packagesRoot, '/patternfly-3');
-const pf4Root = join(packagesRoot, '/patternfly-4');
-const typeGeneral = '0';
-const typePf3 = '3';
-const typePf4 = '4';
 
 function setPackageGenerators(plop) {
   plop.setGenerator('Package', {
@@ -25,17 +20,6 @@ function setPackageGenerators(plop) {
         }
       },
       {
-        type: 'input',
-        name: 'packageType',
-        message: `What type of package is it? Options: ${typeGeneral} (General package), ${typePf3} (Patternfly 3 package), ${typePf4} (Patternfly 4 package)`,
-        validate(input) {
-          const validInput = input === typeGeneral || input === typePf3 || input === typePf4;
-          return validInput
-            ? true
-            : `Type must be one of: ${typeGeneral} (General package), ${typePf3} (Patternfly 3 package), ${typePf4} (Patternfly 4 package)`;
-        }
-      },
-      {
         type: 'confirm',
         name: 'buildsWithBabel',
         message: 'Does this build with Babel?'
@@ -47,15 +31,7 @@ function setPackageGenerators(plop) {
       }
     ],
     actions: answers => {
-      let packageLocation;
-      if (answers.packageType === typeGeneral) {
-        packageLocation = packagesRoot;
-      } else if (answers.packageType === typePf3) {
-        packageLocation = pf3Root;
-      } else {
-        packageLocation = pf4Root;
-      }
-      const packageBaseTemplate = join(packageLocation, `./{{${REMOVE_NPM_SCOPE} name}}/`);
+      const packageBaseTemplate = join(packagesRoot, `./{{${REMOVE_NPM_SCOPE} name}}/`);
       return [
         {
           type: 'add',
